aeb7036e92238ed4433eda18ccd3f2dec73a2058
JP <JullianP1@learn.cssd.ab.ca>
2017-10-10 13:04:47 -0600

    updated program

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace HELLOWORLD
             Console.WriteLine("Write 2 numbers, I'll prove how good my math skills are.");
             float number1 = float.Parse(Console.ReadLine ());
             float number2 = float.Parse(Console.ReadLine());
-            Console.WriteLine (AddTwoNumbers(number1, number1));
+            Console.WriteLine (AddTwoNumbers(number1, number1) + " I'm lit fam.");
         }
         public static float AddTwoNumbers(float n1, float n2) 
         {
