5b5e29c80c64646b5ff1cef9ed4d762a0e9c2521
JP <JullianP1@learn.cssd.ab.ca>
2017-10-10 13:17:10 -0600

    updated program

--- Program.cs
+++ Program.cs
@@ -13,12 +13,16 @@ namespace HELLOWORLD
             Console.WriteLine ("Select an option.\n1.-add\n2.-Subtract\n3.-Multiply\n4.-Divide");
             int option = int.Parse(Console.ReadLine());
 
-            Console.WriteLine (AddTwoNumbers(number1, number1));
+            EvaluateOption (option, number1, number2);
         }
-        public static float AddTwoNumbers(float n1, float n2) 
-        {
+        public static float AddTwoNumbers(float n1, float n2) {
             float result = n1 + n2;
             return result;
+        }
+        public static void EvaluateOption (int option, float n1, float n2) {
+            if(option == 1) {
+                Console.WriteLine("Yoyo my fam I added those and got: " + AddTwoNumbers(n1, n2));
+            }
         }
     }
 }
